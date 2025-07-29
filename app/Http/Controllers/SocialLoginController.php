e3af87cc50d46d0d473214ad30558b35c4943e5f
unknown <raveenjayasanka41@gmail.com>
2025-07-29 21:15:52 +0530

    OAuth part complete (half)

---
+++ app/Http/Controllers/SocialLoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\RedirectResponse;
+class SocialLoginController extends Controller
+{
+    public function toProvider($driver){
+        return Socialite::driver($driver)->redirect();
+    }
+    public function handleCallBack($driver): RedirectResponse{
+       return redirect()->route('dashboard2');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SocialLoginController;
+
+Route::get('/socialite/{driver}',[SocialLoginController::class,'toProvider'])->where('driver','github|google');//route for social login
+Route::get('/auth/{driver}/login',[SocialLoginController::class,'handleCallBack'])->where('driver','github|google');
 
 Route::view('/', 'welcome');
+Route::view('/dashboard2', 'dashboard')
+    ->name('dashboard2');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
