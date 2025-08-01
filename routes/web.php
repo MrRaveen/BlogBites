4e2e5f1d5c954047475825fcbaa47d34c570bcb3
unknown <raveenjayasanka41@gmail.com>
2025-08-01 11:12:03 +0530

    rote changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SocialLoginController;
 use App\Http\Controllers\ProfileController;
@@ -18,13 +17,9 @@ use App\Http\Controllers\BlogController;
 Route::get('/socialite/{driver}',[SocialLoginController::class,'toProvider'])->where('driver','github|google');//route for social login
 Route::get('/auth/{driver}/login',[SocialLoginController::class,'handleCallBack'])->where('driver','github|google');
 //writter endpoints
-
-
-
 Route::view('/', 'welcome');
 Route::view('/dashboard2', 'dashboard')
     ->name('dashboard2');
-
 
 // Route::view('dashboard', 'dashboard')
 //     ->middleware(['auth', 'verified'])
